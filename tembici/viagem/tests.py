5c1336a01fd984f8cd5e4fdb8542d31680027e92
Janaina <nainabarbosa@gmail.com>
2020-04-02 14:36:44 -0300

    fix model name

--- tembici/viagem/tests.py
+++ tembici/viagem/tests.py
@@ -1,7 +1,7 @@
 from django.urls import reverse
 from rest_framework.test import APITestCase, APIClient
 from rest_framework.views import status
-from .models import Trips
+from .models import Trip
 from django.contrib.auth.models import User
 from .serializers import TripsSerializer
 
